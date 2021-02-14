9c7d18f26af6a00247d564347b75f62452f9a8e4
Raghav Agrawal <agrawal0019@hotmail.com>
2021-02-14 21:33:09 +0530

    Add project files.

---
+++ DeliveryMaster/DeliveryMaster/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using DeliveryMaster.ViewModels;
+
+namespace DeliveryMaster.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DeliveryMaster/DeliveryMaster/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DeliveryMaster.Models;
+using DeliveryMaster.ViewModels;
+
+namespace DeliveryMaster.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
